f5d4c887957d4998204a2d939d5dd5832767bb3f
kenil-9dot <kenil.9dot@gmail.com>
2026-02-13 18:22:07 +0530

    Refactor Sidebar and DashboardLayout components for consistency and readability by updating import statements and adjusting formatting.

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { ReactNode, useState } from "react";
-import Sidebar  from "./sidebar";
+import Sidebar from "./Sidebar";
 import { Menu } from "lucide-react";
 
 export default function DashboardLayout({
@@ -8,27 +8,27 @@ export default function DashboardLayout({
 }: {
   children: ReactNode;
 }) {
-    const [open, setOpen] = useState(false);
-    return (
-      <div className="flex h-screen bg-gray-100">
-        <Sidebar open={open} onClose={() => setOpen(false)} />
-  
-        <div className="flex flex-col flex-1">
-          <div className="lg:hidden h-16 flex items-center px-4 border-b border-black/10 bg-white">
-            <button
-              type="button"
-              className="rounded-md p-2 text-slate-700 hover:bg-black/5"
-              aria-label="Open menu"
-              onClick={() => setOpen(true)}
-            >
-              <Menu size={18} />
-            </button>
-            <span className="ml-2 text-sm">Menu</span>
-          </div>
-          <main className="flex-1 flex flex-col overflow-hidden">
-            {children}
-          </main>
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar open={open} onClose={() => setOpen(false)} />
+
+      <div className="flex flex-col flex-1">
+        <div className="lg:hidden h-16 flex items-center px-4 border-b border-black/10 bg-white">
+          <button
+            type="button"
+            className="rounded-md p-2 text-slate-700 hover:bg-black/5"
+            aria-label="Open menu"
+            onClick={() => setOpen(true)}
+          >
+            <Menu size={18} />
+          </button>
+          <span className="ml-2 text-sm">Menu</span>
         </div>
+        <main className="flex-1 flex flex-col overflow-hidden">
+          {children}
+        </main>
       </div>
-    );
+    </div>
+  );
 }
